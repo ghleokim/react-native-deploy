60e69e8d743a4780e02121ddec8df515ff0f864d
leehojun <rainbow_83@naver.com>
2020-02-03 10:57:31 +0900

    modi replies table

--- backend/models/reply.js
+++ backend/models/reply.js
@@ -1,14 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const reply = sequelize.define('reply', {
-    postID: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    writer: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
     content: {
       type: DataTypes.TEXT,
     },
