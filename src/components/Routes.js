8f1d93a56bb9616ed2a391053b6f2bf4fac8dbd3
Leo Kim <gh.leokim@gmail.com>
2020-02-08 21:28:59 +0900

    add pages

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,8 @@
 import React, { } from 'react';
 import { RouteMain } from './routerModules/RouteMain';
+import { RouteLogin } from './routerModules/RouteLogin';
+import { RouteSeller } from './routerModules/RouteSeller';
+import { RouteCustomer } from './routerModules/RouteCustomer';
 import { Router, Switch, Route, Redirect } from './Router';
 
 export const Routes = (Props) => {
@@ -10,6 +13,9 @@ export const Routes = (Props) => {
         <view style={{ height: Props.height, marginTop: Props.headerHeight, marginBottom: Props.footerHeight }} contentContainerStyle={{ flex: 1, flexDirection: 'column', alignItems: 'stretch' }}>
           <Switch>
             <Route exact path='/' component={RouteMain} />
+            <Route exact path='/login' component={RouteLogin} />
+            <Route exact path='/guide/seller' component={RouteSeller} />
+            <Route exact path='/guide/customer' component={RouteCustomer} />
             <Redirect path='*' to='/' />
           </Switch>
         </view>
