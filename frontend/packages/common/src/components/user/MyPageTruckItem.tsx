310b81f1114e03d88b28666511599c8cce166a1a
SteelFox <jhjhyjin@naver.com>
2020-02-10 19:57:26 +0900

    click follow button, move detail page.

--- frontend/packages/common/src/components/user/MyPageTruckItem.tsx
+++ frontend/packages/common/src/components/user/MyPageTruckItem.tsx
@@ -5,6 +5,7 @@ import { Colors } from '../../static/CustomColor';
 import { CustomStyle, CustomText } from '../../static/CustomStyle';
 import axios from 'axios'
 import { mainStoreContext } from '../../store/MainStore';
+import { History, LocationState } from 'history';
 
 interface TruckProps {
   title: String,
@@ -14,18 +15,27 @@ interface TruckProps {
 }
 
 interface Props {
-  truck: TruckProps
+  truck: TruckProps,
+  history: History<LocationState>
 }
 
-export const MyPageTruckItem : React.FC<Props> = ({truck}) => {
+export const MyPageTruckItem : React.FC<Props> = ({truck, history}) => { 
+  const moveToTruckDetail = () => {
+    console.log("clickedTruck : ", truck);
+    history.push(`/trucks/${truck.id}`);
+  }
   return (
-  <View style={styles.truckContainer}>
-    <Image
-      source={{ uri: truck.imgURL ? truck.imgURL : '' }}
-      style={{ position:'absolute', width: '100%', height: 100, borderRadius: 20, borderWidth: 1, borderColor: Colors.gray }}
-      defaultSource={{ uri: `https://picsum.photos/id/${truck.id ? truck.id : 0}/200` }}
-      />
-    <Text style={{ fontWeight: '700', fontSize: 22, backgroundColor: 'rgba(255,255,255,0.5)', alignSelf: 'center', zIndex: 1, paddingHorizontal: 10, paddingVertical: 5}}>{truck.title}</Text>
+  <View>
+    <TouchableOpacity
+      style={styles.truckContainer}
+      onPress={() => moveToTruckDetail()}>
+      <Image
+        source={{ uri: truck.imgURL ? truck.imgURL : '' }}
+        style={{ position:'absolute', width: '100%', height: 100, borderRadius: 20, borderWidth: 1, borderColor: Colors.gray }}
+        defaultSource={{ uri: `https://picsum.photos/id/${truck.id ? truck.id : 0}/200` }}
+        />
+      <Text style={{ fontWeight: '700', fontSize: 22, backgroundColor: 'rgba(255,255,255,0.5)', alignSelf: 'center', zIndex: 1, paddingHorizontal: 10, paddingVertical: 5}}>{truck.title}</Text>
+    </TouchableOpacity>
   </View>
 )}
 
