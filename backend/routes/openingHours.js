82314da863b70070003a71f6d811d3057cb63503
leehojun <rainbow_83@naver.com>
2020-02-13 11:08:09 +0900

    add truck opening hours function

---
+++ backend/routes/openingHours.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const models = require("../models");
+const sequelize = require("sequelize");
+
+router.get('/getMyTime', async function(req, res, next){
+    let result = await models.openingHours.findOne({
+        where: {
+            truckId: req.session.truckId
+        }
+    });
+    res.json(result);
+});
+
+router.get('/getTime/:truckId', async function(req, res, next){
+    let result = await models.openingHours.findOne({
+        where:{
+            truckId: req.params.truckId
+        }
+    });
+    res.json(result);
+});
+
+router.post('/setTime', async function(req, res, next){
+    let result = await models.openingHours.create({
+        truckId: req.session.truckId,
+        beginTime: req.body.beginTime,
+        endTime: req.body.endTime,
+    });
+    res.json(result);
+});
+
+router.put('/updateTime', async function(req, res, next){
+    let result = await models.openingHours.update({
+        beginTime: req.body.beginTime,
+        endTime: req.body.endTime
+    }, {
+        where: {
+            truckId: req.session.truckId
+        }
+    });
+
+    let ret = await models.openingHours.findOne({
+        where: {
+            truckId: req.session.truckId
+        }
+    });
+    
+    res.json(ret);
+});
+
+
+module.exports = router;
