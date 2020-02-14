0db4cd607d61bd626c34329c65fdbc5999c635ec
Leo Kim <gh.leokim@gmail.com>
2020-02-14 10:02:12 +0900

    Add Banner scroll

--- frontend/packages/common/src/components/main/Modal.tsx
+++ frontend/packages/common/src/components/main/Modal.tsx
@@ -4,18 +4,6 @@ import { View, Text, Image, ScrollView, Animated, TouchableOpacity } from 'react
 import { BannerStoreContext } from '../../store/BannerStore';
 import { observer } from 'mobx-react-lite';
 import { mainStoreContext } from '../../store/MainStore';
-// <<<<<<< HEAD
-
-// interface Props {
-//   title?: string,
-//   updatedAt?: string,
-//   content?: string,
-//   imgURL?: string,
-//   Child?: React.FC
-// }
-
-// export const Modal: React.FC<Props> = observer(({title, content, imgURL, updatedAt, Child}) => {
-// =======
 import { CustomText } from '../../static/CustomStyle';
 
 interface Notice {
@@ -32,16 +20,16 @@ interface Props {
 }
 
 export const Modal: React.FC<Props> = observer(({ notice, imgURL }) => {
-// >>>>>>> dev-front
   const MainStore = useContext(mainStoreContext);
   const BannerStore = useContext(BannerStoreContext);
+  const defaultTopOffset = MainStore.headerHeight
 
   const [modalState, setModalState] = useState(true);
   const [topOffset] = useState(new Animated.Value(MainStore.screenHeight));
 
   useEffect(() => {
     Animated.timing(topOffset, {
-      toValue: modalState ? 0 : MainStore.screenHeight,
+      toValue: modalState ? defaultTopOffset : MainStore.screenHeight,
       duration: 300
     }).start()
   },[modalState])
@@ -49,19 +37,10 @@ export const Modal: React.FC<Props> = observer(({ notice, imgURL }) => {
   if (!!imgURL) { Image.getSize(imgURL, (width, height)=>{BannerStore.imgHeight = height * MainStore.screenWidth / width}, (err)=>{console.log(err)})}
   console.log(!!imgURL, imgURL, BannerStore.imgHeight)
 
-// <<<<<<< HEAD
-  
-
-//   return (
-//     <Animated.View style={{position: 'absolute', top: topOffset, width: '100%', height: '100%', backgroundColor: '#ffffff', zIndex: 2}}>
-//       <View style={{height: 40, borderBottomColor: '#a090c0', borderBottomWidth: 1, justifyContent: 'center'}}>
-//         <TouchableOpacity onPressOut={()=>{setModalState(false); setTimeout(()=>{BannerStore.active=false}, 500)}} style={{flex: 1, flexDirection:'row', justifyContent: 'space-between'}}>
-// =======
   return (
     <Animated.View style={{position: 'absolute', top: topOffset, width: '100%', height: '100%', backgroundColor: '#ffffff', zIndex: 2}}>
       <View style={{height: 40, borderBottomColor: '#a090c0', borderBottomWidth: 1, justifyContent: 'center'}}>
         <TouchableOpacity onPressOut={()=>{setModalState(false); setTimeout(()=>{BannerStore.active=false; BannerStore.pageIndex = -1}, 500)}} style={{flex: 1, flexDirection:'row', justifyContent: 'space-between'}}>
-{/* >>>>>>> dev-front */}
           <View style={{width: 20}}></View>
           <Text style={{alignSelf: 'center'}}>공지사항</Text>
           <Image style={{ marginRight: 10, width: 20, height: 20, alignSelf: 'center'}} source={require('@foodtruckmap/common/src/static/icon_processed/noun_Close_1015372.png')}/>
@@ -73,11 +52,6 @@ export const Modal: React.FC<Props> = observer(({ notice, imgURL }) => {
           <Image style={{height: '100%', width: '100%', resizeMode: 'contain'}} source={{uri: imgURL}} defaultSource={{uri: 'https://picsum.photos/200'}}/>
           </View>
           : <View>
-{/* <<<<<<< HEAD
-            <Text>{title}</Text>
-            <Text>{updatedAt}</Text>
-            <Text>{content}</Text>
-======= */}
               <View style={{height: 80, paddingHorizontal: 15, justifyContent: 'center', borderBottomColor: '#c0c0c0', borderBottomWidth: 1}}>
                 <Text style={[CustomText.title, {fontSize: 16}]}>{notice.title}</Text>
                 <Text style={[CustomText.body, {fontSize: 14, color: '#606060'}]}>{notice.updatedAt}</Text>
@@ -85,7 +59,6 @@ export const Modal: React.FC<Props> = observer(({ notice, imgURL }) => {
               <View style={{paddingTop: 12, paddingHorizontal: 15}}>
                 <Text style={[CustomText.body, {fontSize: 14}]}>{notice.content}</Text>
               </View>
-{/* >>>>>>> dev-front */}
           </View> 
         }
       </ScrollView>
