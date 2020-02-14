518a7ea2682dbba070ad745faf944e1939f77bb5
Geonho Kim / Leo Kim <gh.leokim@gmail.com>
2020-02-14 14:44:57 +0900

    401 error handling

--- frontend/packages/common/src/index.tsx
+++ frontend/packages/common/src/index.tsx
@@ -21,6 +21,21 @@ axios.defaults.baseURL=HTTPS_AWS;
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.withCredentials = true;
+axios.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  if (401 === error.response.status && '/users/login' === error.response.config.url ) {
+    alert('잘못된 로그인 정보입니다. 다시 시도해주십시오.')
+    return Promise.reject(error)
+  } else if (401 === error.response.status) {
+    console.log(error.response)
+    alert('로그인 후 이용 가능한 기능입니다.')
+    history.go(-1)
+    return Promise.reject(error);
+  } else {
+      return Promise.reject(error);
+  }
+});
 
 export const App: React.FC = observer(() => {
   const mainStore = useContext(mainStoreContext);
