c2269e66e11dc7c6c6c6a391aeb1d396fd835cff
Geonho Kim / Leo Kim <gh.leokim@gmail.com>
2020-02-17 19:33:26 +0900

    add login routes after unauthorized login

--- src/components/routerModules/RouteLogin.js
+++ src/components/routerModules/RouteLogin.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { LoginForm } from '../pages/LoginForm';
 
-export const RouteLogin = () => {
-  return <LoginForm />
+export const RouteLogin = ({history}) => {
+  return <LoginForm history={history}/>
 }
