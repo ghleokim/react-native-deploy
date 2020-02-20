272e3b7e3d5133b6b6b53668b4e64a85e7187cc7
Geonho Kim / Leo Kim <gh.leokim@gmail.com>
2020-02-20 14:12:32 +0900

    History redesign

--- packages/common/src/components/foodtruckDetail/TruckSaleHistoryDetail.tsx
+++ packages/common/src/components/foodtruckDetail/TruckSaleHistoryDetail.tsx
@@ -44,11 +44,11 @@ export default (props: IProps) => {
     return (
       <TouchableOpacity onPress={() => handleHistory(history.latitude, history.longitude)} key={history.id}>
       <View style={styles.historyRow}>
-        <View style={{flex: 1, flexDirection: 'row', paddingVertical: 4, }}>
+        <View style={{flex: 1, flexDirection: 'row', paddingVertical: 4, marginLeft: 8, borderLeftColor: '#68a168', borderLeftWidth: 2, borderStyle: 'dotted'}}>
           <View style={{flex: 10}}></View>
-          <View style={{flex: 90, borderWidth: 1, borderColor: '#CCCCCC',borderRadius: 5, flexDirection: 'row', paddingVertical: 7}}>
-            <Text style={{flex: 1, textAlign: 'left', paddingLeft: 10, color: '#AAAAAA', letterSpacing: 2}}>{moment(history.beginTime).format('hh:mm')} ~ {moment(history.endTime).format('hh:mm')}</Text>
-            <Text style={{flex: 1, textAlign: 'right', paddingRight: 10, color: '#AAAAAA'}}>{`${String(history.area2).trim().substring(history.area2.lastIndexOf(" ")+1,history.area2.length)} ${history.area3}`}</Text>
+          <View style={{flex: 90, borderBottomWidth: 1, borderColor: '#808080', flexDirection: 'row', paddingVertical: 3}}>
+            <Text style={{flex: 1, textAlign: 'left', paddingLeft: 10, color: '#808080', letterSpacing: 2}}>{moment(history.beginTime).format('hh:mm')} ~ {moment(history.endTime).format('hh:mm')}</Text>
+            <Text style={{flex: 1, textAlign: 'right', paddingRight: 10, color: '#808080'}}>{`${String(history.area2).trim().substring(history.area2.lastIndexOf(" ")+1,history.area2.length)} ${history.area3}`}</Text>
             </View>
           </View>
       </View>
@@ -81,8 +81,8 @@ export default (props: IProps) => {
     return (
       <View style={styles.historiesBox} key={histories[0]}>
           <View style={{flexDirection: 'row'}}>            
-            <Text style={{flex: 5}}></Text>
-            <Text style={styles.historiesBoxDate}>{YYYYMMDD_korean_day(histories[0])}</Text>
+            {/* <Text style={{flex: 5}}></Text> */}
+            <Text style={styles.historiesBoxDate}>•  {YYYYMMDD_korean_day(histories[0])}</Text>
           </View>
         {histories[1].map(history => historyRow(history))}
       </View>        
@@ -100,6 +100,7 @@ export default (props: IProps) => {
       <View style={{paddingVertical: 15}}></View>
 
       <Text style={styles.truckInfoSubTitle}>히스토리</Text>
+      <Text style={{color: '#808080', fontSize: 13, fontWeight: '500', paddingLeft: 4, paddingBottom: 8}}> 각 항목을 선택하면 해당 위치를 확인하실 수 있습니다.</Text>
         <View style={styles.historyBody}>
           {
             truckHistories.length === 0 
@@ -131,17 +132,19 @@ const styles = StyleSheet.create({
   truckInfoSubTitle: {
     fontSize: 16,
     fontWeight: '800',
-    paddingVertical: 10
+    paddingTop: 10,
+    paddingBottom: 4
   },
   historyRow: {
     paddingVertical: 4,
     paddingHorizontal: 8,
   },
   historiesBox: {
-    paddingVertical: 7,
+    // paddingVertical: 7,
   },
   historiesBoxDate: {
     fontSize: 15,
     flex: 80,
+    paddingLeft: 12
   },
 })
